41c1c846114c6d1bdde7c7555487cb2fc05fc710
m0L3cuL3 <skidrowloler@gmail.com>
2022-03-13 19:17:25 +0000

    updated course syllabus section

---
+++ course_syllabus_builder/course_syllabus_builder/doctype/course_syllabus/course_syllabus.js
@@ -0,0 +1,57 @@
+// Copyright (c) 2022, Sean Baang and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Course Syllabus', {
+	refresh: function(frm) {
+		let college = frm.doc.college_name;
+		  
+			if(college){
+				frappe.call({
+					method: "course_syllabus_builder.course_syllabus_builder.doctype.course_syllabus.course_syllabus.get_college_departments",
+					args: {college: college}
+				}).done((r) => {
+					console.log(r.message)
+	
+					let dept = []
+				
+					$.each(r.message, function(_i, e){
+						console.log(e)
+						dept[_i] = e.department
+					})
+					
+					frm.set_df_property('college_department', 'options', dept)
+					refresh_field("college_department")
+			
+				})
+		   }
+	}
+})
+
+frappe.ui.form.on('Course Syllabus', {
+
+	college_name: function(frm){
+
+		let college = frm.doc.college_name;
+	  
+		if(college){
+			frappe.call({
+				method: "course_syllabus_builder.course_syllabus_builder.doctype.course_syllabus.course_syllabus.get_college_departments",
+				args: {college: college}
+			}).done((r) => {
+				console.log(r.message)
+
+				let dept = []
+			
+				$.each(r.message, function(_i, e){
+					console.log(e)
+					dept[_i] = e.department
+				})
+				
+				frm.set_df_property('college_department', 'options', dept)
+				refresh_field("college_department")
+		
+			})
+	   }
+	  
+	}
+});
