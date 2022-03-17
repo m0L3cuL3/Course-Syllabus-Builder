b4793784b2c5e5d0469b2867925b829b4850cfa0
m0L3cuL3 <skidrowloler@gmail.com>
2022-03-17 10:34:27 +0000

    added program learning outcomes doctype

---
+++ course_syllabus_builder/course_syllabus_builder/doctype/program_learning_outcomes/program_learning_outcomes.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022, Sean Baang and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class ProgramLearningOutcomes(Document):
+	pass
+
